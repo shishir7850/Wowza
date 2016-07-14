b56ea74d238d45e0e80ef82a4c589c6841239056
Shishir Lakkadi <slakkadi@purdue.edu>
2016-07-14 15:05:11 +0530

    Initial commit

---
+++ app/src/main/java/edu/purdue/shishir/wowza/AppConfig.java
@@ -0,0 +1,11 @@
+package edu.purdue.shishir.wowza;
+
+/**
+ * Created by Shishir on 05/07/16.
+ */
+
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://10.96.66.20:1935/live/myStream";
+    public static final String PUBLISHER_USERNAME = "admin1";
+    public static final String PUBLISHER_PASSWORD = "admin";
+}
