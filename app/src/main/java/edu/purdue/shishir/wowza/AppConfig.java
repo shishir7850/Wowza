d67bc43ad4ac9bd2fb72661b20b8a56a56201983
Shishir Lakkadi <slakkadi@purdue.edu>
2016-07-20 14:10:50 +0530

    Fourth and Stable Version

--- app/src/main/java/edu/purdue/shishir/wowza/AppConfig.java
+++ app/src/main/java/edu/purdue/shishir/wowza/AppConfig.java
@@ -5,7 +5,7 @@ package edu.purdue.shishir.wowza;
  */
 
 public class AppConfig {
-    public static final String STREAM_URL = "rtsp://10.96.66.139:1935/live/myStream";
-    public static final String PUBLISHER_USERNAME = "admin1";
+    public static final String STREAM_URL = "rtsp://10.96.66.201:1935/live/myStream";
+    public static final String PUBLISHER_USERNAME = "admin";
     public static final String PUBLISHER_PASSWORD = "admin";
 }
